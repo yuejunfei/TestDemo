e3988ec1d572408b476e3b0919992f25e0f7346e
“yuejunfei” <451155047@qq.coom>
2019-11-18 15:52:09 +0800

    ...

---
+++ 21_drag_nestedscroll/src/main/java/com/example/a21_drag_nestedscroll/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.a21_drag_nestedscroll;
+
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.drag_helper_grid_view);
+  }
+}
